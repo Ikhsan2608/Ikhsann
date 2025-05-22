5a7a2dc23f05de0881db051726b96e6b1082430b
Ikhsan2608 <ikhsannuriman76@gmail.com>
2025-05-22 09:04:17 +0700

    Update and rename 1_dashboard.py to 1_halaman pertama.py

--- pages/1_dashboard.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Dashboard Streamlit", layout="wide")
-
-st.title("Selamat Datang di Dashboard Streamlit")
-st.write("""
-Ini adalah halaman utama dari dashboard Streamlit.
-Gunakan sidebar di kiri untuk menavigasi ke halaman lainnya.
-""")
